4339683ee7fca803e806ac3f9e24c8597b94e472
Dakota Smith <smith.dakota52@gmail.com>
2018-06-28 13:38:36 -0700

    webpage now displays properly

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-$(document).ready(function(){
-    $.getJSON("users.json", function(result){
-        $.each(result, function(i, field){
-            $("div").append(JSON.stringify(field['id']) + ') ' + JSON.stringify(field['id','username']) + '<br> ' + JSON.stringify(field['id','name']) + '<br> ' + JSON.stringify(field['id','bio']) + '<br> ' +  "<p>");
-        });
-    });
-    $.getJSON("posts.json", function(postResult){
-        $.each(postResult, function(i, field){
-            $("div").append(JSON.stringify(field['thread']) + ') ' + JSON.stringify(field['text']) + ' ' + ' <br>' + ' <p>')
-        });
-    });
-    $.getJSON("threads.json", function(threadsResult){
-        $.each(threadsResult, function(i, field){
-            $("div").append(JSON.stringify(field['id']) + ' ' + JSON.stringify(field['title'])+ ' <br>' + ' <p>')
-        });
-    });
+$('#form1').submit(function(event){
+    createTables('https://p848p29pz3.execute-api.us-west-2.amazonaws.com/test/users', event)
 });
+
+$('#form3').submit(function(event){
+    createTables('https://p848p29pz3.execute-api.us-west-2.amazonaws.com/test/posts', event)
+});
+
+$('#form2').submit(function(event){
+    createTables('https://p848p29pz3.execute-api.us-west-2.amazonaws.com/test/threads', event)
+});
+
+function createTables(myurl, event){
+    $('#table-head').empty();
+    $('#table-body').empty();
+    console.log('hi');
+    event.preventDefault();
+    $.ajax({
+        url:myurl,
+        type:'GET',
+        dataType:'json',
+        success:function(data){
+            datakeys = Object.keys(data[0]);
+            tableHeaderMarkup  = "<tr>"
+            for(var j=0; j<datakeys.length; j++)
+                tableHeaderMarkup +="<th>"+datakeys[j]+"</th>";
+            tableHeaderMarkup += "</tr>";
+
+            $('#table').append('<thead id="table-head"></thead>');
+            $('#table-head').append(tableHeaderMarkup);
+            $('#table').append('<tbody id="table-body"></tbody>');
+
+            for(var i=0; i < data.length; i++){
+                var tableBodyMarkup = "<tr>";
+                $.each(data[i], function(key,element){
+                    tableBodyMarkup += "<th>"+element+"</th>";
+                })
+                tableBodyMarkup += "</th>";
+                $('#table-body').append(tableBodyMarkup);
+            }
+        }
+    })
+}
